63797c7325d92403e70abce2b9e30ef2981838db
Anas Bin Ajaz <123629492+BazazAnas@users.noreply.github.com>
2026-09-02 23:15:40 +0530

    Time: 2 ms (5.08%) | Memory: 8.6 MB (71.63%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.c
@@ -0,0 +1,13 @@
+bool isSubsequence(char* s, char* t) {
+    int i = 0;
+    int j = 0;
+    int sLen = strlen(s);
+    int tLen = strlen(t);
+    while (i < sLen && j < tLen) {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+    return i == sLen;
+}
