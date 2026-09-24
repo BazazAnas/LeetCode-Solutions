e83e0cbdf0a7b2d2ee6eea7ee17ebfac77e1a4c2
Anas Bin Ajaz <123629492+BazazAnas@users.noreply.github.com>
2026-09-24 22:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (37.82%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.c
@@ -0,0 +1,19 @@
+int digitSum(int num) {
+    int sum = 0;
+    if (num == 0)
+        return 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int smallestIndex(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        if (digitSum(nums[i]) == i) {
+            return i;
+        }
+    }
+    return -1;
+}
